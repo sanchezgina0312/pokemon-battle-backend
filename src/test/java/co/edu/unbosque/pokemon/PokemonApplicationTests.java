abe275644a4337b0b16c817c7b24b7f3f57574f7
sanchezgina0312 <sanchez.gina0312@gmail.com>
2026-04-04 23:12:53 -0500

    Creación Repositorio Pokemon Back
    Creación Repositorio Pokémon Back

---
+++ src/test/java/co/edu/unbosque/pokemon/PokemonApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.pokemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
