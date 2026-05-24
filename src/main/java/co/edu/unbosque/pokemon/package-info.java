1a0434310cf3224fe4d565b50feae314751cfa7d
avillarreala <tatikoavila23@gmail.com>
2026-05-24 12:34:33 -0500

    package info

---
+++ src/main/java/co/edu/unbosque/pokemon/package-info.java
@@ -0,0 +1,19 @@
+/**
+ * Sistema Pokémon desarrollado para la gestión
+ * de usuarios, pokémon, tiendas, inventarios,
+ * ataques y demás funcionalidades de la aplicación.
+ * 
+ * <p>
+ * Este paquete principal agrupa todos los componentes
+ * del proyecto, incluyendo controladores, servicios,
+ * entidades, repositorios, configuraciones, seguridad,
+ * excepciones y utilidades necesarias para el correcto
+ * funcionamiento del sistema.
+ * </p>
+ * 
+ * @author Gina Buitrago
+ * @author Natalia Diaz
+ * @author Gina Sanchez
+ * @author Angie Villarreal
+ */
+package co.edu.unbosque.pokemon;
---
+++ src/main/java/co/edu/unbosque/pokemon/service/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * Contiene las clases de servicio encargadas de la lógica
+ * de negocio del sistema Pokémon.
+ * 
+ * <p>
+ * En este paquete se gestionan las operaciones relacionadas
+ * con usuarios, pokémon, tiendas, inventarios, ataques
+ * y demás funcionalidades principales de la aplicación.
+ * </p>
+ * 
+ * @author Gina Buitrago
+ * @author Natalia Diaz
+ * @author Gina Sanchez
+ * @author Angie Villarreal
+ */
+package co.edu.unbosque.pokemon.service;
